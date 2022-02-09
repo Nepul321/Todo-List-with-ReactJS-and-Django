ef9325f02ba16932a6bf231ba8423dca61bd8455
Nepul Kahandawa <nepulkahandawa7f@gmail.com>
2022-02-08 21:33:48 -0500

    Create a TodosListView

--- base/urls.py
+++ base/urls.py
@@ -1,10 +1,12 @@
 from unicodedata import name
 from .views import (
-  APIBaseView
+  APIBaseView,
+  TodosListView
 )
 
 from django.urls import path
 
 urlpatterns = [
     path('', APIBaseView, name="api-base"),
+    path('todos/', TodosListView, name="todos-list-view")
 ]
--- base/views.py
+++ base/views.py
@@ -1,6 +1,18 @@
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from .models import (
+    Todo
+)
+
+from .serializers import TodoSerializer
 
 @api_view(['GET'])
 def APIBaseView(request):
     return Response("API BASE POINT", status=200)
+
+@api_view(['GET'])
+def TodosListView(request):
+    qs = Todo.objects.filter(completed=False)
+    serializer = TodoSerializer(qs, many=True)
+    data = serializer.data
+    return Response(data, status=200)
