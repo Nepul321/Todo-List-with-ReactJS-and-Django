37cfcce195045806759f0c55342ffd70bf7a8913
Nepul Kahandawa <nepulkahandawa7f@gmail.com>
2022-02-13 19:43:28 -0500

    Add a user field for todos

--- base/models.py
+++ base/models.py
@@ -1,7 +1,9 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 class Todo(models.Model):
     title = models.CharField(max_length=255)
+    user = models.ForeignKey(User, blank=True, on_delete=models.CASCADE, null=True)
     completed = models.BooleanField(default=False)
     datetime = models.DateTimeField(auto_now_add=True)
 
